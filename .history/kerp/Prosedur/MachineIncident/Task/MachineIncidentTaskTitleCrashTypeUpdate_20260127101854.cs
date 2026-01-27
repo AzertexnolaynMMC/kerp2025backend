77aaf1d6385bca8a17132115481d61c69ff465c0
AzertexnolaynMMC <instgram.cominfi@gmail.com>
2026-01-27 10:20:59 +0400

    27.01.2026

---
+++ .history/kerp/Prosedur/MachineIncident/Task/MachineIncidentTaskTitleCrashTypeUpdate_20260127101854.cs
@@ -0,0 +1,10 @@
+﻿namespace kerp.Prosedur.MachineIncident.MachineIncidentTask
+{
+    public class MachineIncidentTaskTitleCrashTypeUpdate
+    {
+        public string Title { get; set; }   // NVARCHAR(255)
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int CrashTypeId { get; set; }
+    }
+}
