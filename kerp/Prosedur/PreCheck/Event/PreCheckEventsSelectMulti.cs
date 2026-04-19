6697cb489ec66bde90a527f9f81afdd34c23d8bf
AzertexnolaynMMC <instgram.cominfi@gmail.com>
2026-04-19 18:37:49 +0400

    19.04.2026

--- kerp/Prosedur/PreCheck/Event/PreCheckEventsSelectMulti.cs
+++ kerp/Prosedur/PreCheck/Event/PreCheckEventsSelectMulti.cs
@@ -2,5 +2,11 @@
 {
     public class PreCheckEventsSelectMulti
     {
+        public int Id { get; set; }
+        public int PreCheckId { get; set; }
+        public int UserId { get; set; }
+        public DateTime EventDate { get; set; }
+        public int EventType { get; set; }
+        public required string UserFullName { get; set; }
     }
 }
